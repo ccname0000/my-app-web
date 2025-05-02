2adb79adae91354d7d8ce24bcd7016e605b5bd5e
ccname0000 <0908788773a@gmail.com>
2025-05-02 17:25:40 +0800

    使用useEffect來加一個副影響 在第七行的useEffect第二個參數使用hoveredItem 這樣可以讓每次hoveredItem改變的時候都會觸發這個副影響

--- src/app/component/NavThreeItems/NavItem2.js
+++ src/app/component/NavThreeItems/NavItem2.js
@@ -1,9 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-export default function NavItem2({ setHoveredItem }) {
+export default function NavItem2({ setHoveredItem, hoveredItem }) {
+  const [isVisible, setIsVisible] = useState(false); // 控制透明度的狀態
+
+  useEffect(() => {
+    let timeout;
+    if (hoveredItem === "What We Do") {
+      // 當 hoveredItem 為 "What We Do" 時，延遲顯示
+      timeout = setTimeout(() => setIsVisible(true), 150); // 延遲 150 毫秒
+    } else {
+      // 當 hoveredItem 為其他值時，延遲隱藏
+      setHoveredItem(false); // 延遲 150 毫秒
+    }
+    return () => clearTimeout(timeout); // 清除定時器，避免內存泄漏
+  }, [hoveredItem]);
+
+  const NavItem2Style = `
+    absolute top-[150px] w-full h-[400px] bg-gray-200 
+    transition-opacity duration-500 ease-in-out 
+    ${isVisible ? "opacity-100" : "opacity-0 pointer-events-none"}
+  `;
+
   return (
     <div
-      className="absolute top-[100px] w-full h-[200px] bg-gray-200 transition-opacity duration-300 ease-in-out opacity-100"
+      className={NavItem2Style}
       onMouseEnter={() => setHoveredItem("What We Do")} // 保持開啟
       onMouseLeave={() => setHoveredItem(null)} // 滑出後隱藏
     >
