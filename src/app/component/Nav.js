9bc5089854178683c284a648eb92d4bd809e9a86
ccname0000 <0908788773a@gmail.com>
2025-05-02 17:29:56 +0800

    把logo換成直接用a標籤 因為效果一樣 logo的區域丟在更下面 因為怕被navitem的區塊擋住 把navitem2這個component多丟了一個參數 hoveredItem 為了能夠保證其他三個component可以有漸入漸出的效果

--- src/app/component/Nav.js
+++ src/app/component/Nav.js
@@ -15,14 +15,11 @@ export default function Nav({ NavItem, MobileNav }) {
   const NavBarItemStyle =
     "absolute md:hidden top-[100px] right-0 w-[250px] bg-gray-800 text-white shadow-md flex flex-col p-4 space-y-4 transform transition-transform duration-500 ease-in-out";
   const LogoStyle =
-    "relative lg:text-5xl text-3xl font-bold top-[40%] left-[20%] hover:text-yellow-700 duration-300 cursor-pointer";
+    "absolute lg:text-5xl text-3xl font-bold top-[5%] left-[20%] hover:text-yellow-700 duration-300 cursor-pointer";
 
   return (
     <>
-      <div className="w-full lg:h-[150px] h-[100px] bg-yellow-100">
-        <button className={LogoStyle}>
-          <a href="#">Logo</a>
-        </button>
+      <div className="w-full lg:h-[150px] h-[100px]">
         {/* 漢堡按鈕 - 手機版 */}
         <div className="absolute right-[6%] top-[7%] md:hidden">
           <button
@@ -50,25 +47,28 @@ export default function Nav({ NavItem, MobileNav }) {
           ))}
         </div>
         {/* 導覽列 - 桌面版 */}
-        <div className=" hidden md:flex justify-end relative">
+        <div className="hidden md:flex justify-end relative space-x-30 right-[4%] top-[35%]">
           {NavItem.map((item, i) => (
             <a
               key={i}
               href="#"
-              className="px-14 pb-20 text-2xl hover:text-orange-400 duration-300 cursor-pointer"
+              className="text-2xl pb-20 hover:text-orange-400 duration-300 cursor-pointer"
               onMouseEnter={() => setHoveredItem(item)} // 設置當前懸停的項目
               onMouseLeave={() => setHoveredItem(null)} // 清除懸停的項目
             >
               {item}
             </a>
           ))}
-          {/* 根據 hoveredItem 顯示對應的組件 */}
-          {hoveredItem === "What We Do" && (
-            <NavItem2 setHoveredItem={setHoveredItem} />
-          )}
-          {hoveredItem === "Her Story" && <NavItem3 />}
-          {hoveredItem === "Library" && <NavItem4 />}
         </div>
+        {/* 根據 hoveredItem 顯示對應的組件 */}
+        <a className={LogoStyle} href="#">
+          logo
+        </a>
+        {hoveredItem === "What We Do" && (
+          <NavItem2 setHoveredItem={setHoveredItem} hoveredItem={hoveredItem} />
+        )}
+        {hoveredItem === "Her Story" && <NavItem3 />}
+        {hoveredItem === "Library" && <NavItem4 />}
       </div>
     </>
   );
