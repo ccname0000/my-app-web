0f601306e0ae8d6e32d8c1b4d3dd36ff2b3129a1
ccname0000 <0908788773a@gmail.com>
2025-05-04 20:36:59 +0800

    改名讓檔名更直觀可以知道這個js檔是幹嘛的

--- src/app/component/NavThreeItems/NavItem3.js
+++
@@ -1,3 +0,0 @@
-export default function NavItem3() {
-  return <></>;
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Nav from "./component/Nav";
-import NavItem2 from "./component/NavThreeItems/NavItem2";
 export default function Home() {
   const NavItems = ["Who we are", "What We Do", "Her Story", "Library"];
   const smNavItems = [
